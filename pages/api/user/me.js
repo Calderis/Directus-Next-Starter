f6b6401f9ca056f55527b397c10facaa5cf2525a
Calderis <r.wetteren@gmail.com>
2021-06-29 16:43:33 +0200

    Feature : User settings page forms

--- pages/api/user/me.js
+++ pages/api/user/me.js
@@ -1,7 +1,16 @@
 import { directus } from "/utils/directus";
 
 export default async function handler(req, res) {
-  directus.users.me.update(req.body)
-    .then(data => res.status(200).json(data))
-    .catch(res.status(401).json);
+  switch (req.method) {
+    default:
+      directus.users.me.read()
+        .then(data => res.status(200).json(data))
+        .catch(res.status(404).json);
+      break;
+    case "POST":
+      directus.users.me.update(req.body)
+        .then(data => res.status(200).json(data))
+        .catch(res.status(401).json);
+      break;
+  }
 }
--- pages/user/settings.js
+++ pages/user/settings.js
@@ -11,11 +11,29 @@ class UserSettings extends Component {
 
     const { session, myself } = props;
 
-    console.log("session", session);
-
     this.state = {
-      user: session.user
+      user: {},
+      credentialForm: "idle",
+      profileForm: "idle",
+      personnalForm: "idle",
     };
+  }
+
+  componentDidMount() {
+    axios.get("/api/user/me")
+      .then(this.setUser)
+      .catch(console.error);
+  }
+
+  setUser = ({ data }) => this.setState({ user: data });
+
+  saveCredentialForm = async event => {
+    event.preventDefault();
+    const { password } = event.target;
+
+    axios.post("/api/user/me", { password: password.value })
+      .then(this.setUser)
+      .catch(console.error);
   }
 
   savePersonnalForm = async event => {
@@ -26,7 +44,19 @@ class UserSettings extends Component {
       first_name: first_name.value,
       last_name: last_name.value
     })
-      .then(console.log)
+      .then(this.setUser)
+      .catch(console.error);
+  }
+
+  saveProfileForm = async event => {
+    event.preventDefault();
+    const { title, description } = event.target;
+
+    axios.post("/api/user/me", {
+      title: title.value,
+      description: description.value
+    })
+      .then(this.setUser)
       .catch(console.error);
   }
 
@@ -42,6 +72,92 @@ class UserSettings extends Component {
       <Layout title="User settings">
         <Protected>
           <div className="mt-10 sm:mt-0">
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              <div className="md:col-span-1">
+                <div className="px-4 sm:px-0">
+                  <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+                  <p className="mt-1 text-sm text-gray-600">
+                    This information will be displayed publicly so be careful what you share.
+                  </p>
+                </div>
+              </div>
+              <div className="mt-5 md:mt-0 md:col-span-2">
+                <form onSubmit={this.saveProfileForm}>
+                  <div className="shadow sm:rounded-md sm:overflow-hidden">
+                    <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                      <div>
+                        <label className="block text-sm font-medium text-gray-700">Photo</label>
+                        <div className="mt-1 flex items-center">
+                          <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+                            <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                            </svg>
+                          </span>
+                          <button
+                            type="button"
+                            className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                            Change
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="col-span-6 sm:col-span-4">
+                        <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                          Pseudo
+                        </label>
+                        <input
+                          defaultValue={user.title}
+                          type="text"
+                          name="title"
+                          placeholder="Anonymous"
+                          id="title"
+                          autoComplete="title"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                      </div>
+
+                      <div>
+                        <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                          About
+                        </label>
+                        <div className="mt-1">
+                          <textarea
+                            defaultValue={user.description}
+                            id="description"
+                            name="description"
+                            rows={3}
+                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                            placeholder="I am so brave"
+                            defaultValue={''}
+                            />
+                        </div>
+                        <p className="mt-2 text-sm text-gray-500">
+                          Brief description for your profile.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden sm:block" aria-hidden="true">
+            <div className="py-5">
+              <div className="border-t border-gray-200" />
+            </div>
+          </div>
+
+          <div>
             <div className="md:grid md:grid-cols-3 md:gap-6">
               <div className="md:col-span-1">
                 <div className="px-4 sm:px-0">
@@ -140,7 +256,7 @@ class UserSettings extends Component {
                 </div>
               </div>
               <div className="mt-5 md:mt-0 md:col-span-2">
-                <form action="#" method="POST">
+                <form onSubmit={this.saveCredentialForm}>
                   <div className="shadow overflow-hidden sm:rounded-md">
                     <div className="px-4 py-5 bg-white sm:p-6">
                       <div className="grid grid-cols-6 gap-6">
@@ -159,92 +275,6 @@ class UserSettings extends Component {
                             Just fill the input and save to update your password
                           </p>
                         </div>
-                      </div>
-                    </div>
-                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                      <button
-                        type="submit"
-                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                        Save
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-
-          <div className="hidden sm:block" aria-hidden="true">
-            <div className="py-5">
-              <div className="border-t border-gray-200" />
-            </div>
-          </div>
-
-          <div>
-            <div className="md:grid md:grid-cols-3 md:gap-6">
-              <div className="md:col-span-1">
-                <div className="px-4 sm:px-0">
-                  <h3 className="text-lg font-medium leading-6 text-gray-900">Profile</h3>
-                  <p className="mt-1 text-sm text-gray-600">
-                    This information will be displayed publicly so be careful what you share.
-                  </p>
-                </div>
-              </div>
-              <div className="mt-5 md:mt-0 md:col-span-2">
-                <form action="#" method="POST">
-                  <div className="shadow sm:rounded-md sm:overflow-hidden">
-                    <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                      <div>
-                        <label className="block text-sm font-medium text-gray-700">Photo</label>
-                        <div className="mt-1 flex items-center">
-                          <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                            <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                          </span>
-                          <button
-                            type="button"
-                            className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                            Change
-                          </button>
-                        </div>
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-4">
-                        <label htmlFor="title" className="block text-sm font-medium text-gray-700">
-                          Pseudo
-                        </label>
-                        <input
-                          defaultValue={user.title}
-                          type="text"
-                          name="title"
-                          placeholder="Anonymous"
-                          id="title"
-                          autoComplete="title"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div>
-                        <label htmlFor="about" className="block text-sm font-medium text-gray-700">
-                          About
-                        </label>
-                        <div className="mt-1">
-                          <textarea
-                            defaultValue={user.description}
-                            id="about"
-                            name="about"
-                            rows={3}
-                            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                            placeholder="I am so brave"
-                            defaultValue={''}
-                            />
-                        </div>
-                        <p className="mt-2 text-sm text-gray-500">
-                          Brief description for your profile.
-                        </p>
                       </div>
                     </div>
                     <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
