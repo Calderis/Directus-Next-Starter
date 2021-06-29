c43548d27e1a3926e52cf07b1c9f52df7adf1138
Calderis <r.wetteren@gmail.com>
2021-06-29 13:37:26 +0200

    add user image

--- components/header.js
+++ components/header.js
@@ -168,6 +168,9 @@ export default function Header () {
                       <>
                         <div>
                           <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                            <div className="flex-shrink-0 h-5 w-5 mr-2">
+                              <img className="h-5 w-5 rounded-full" src={session.user.image} alt="" />
+                            </div>
                             {session.user.name}
                             <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
                           </Menu.Button>
