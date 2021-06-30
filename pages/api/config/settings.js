c7ccdcbae6b8fca63aff1b118a428513cf3ac78a
Calderis <r.wetteren@gmail.com>
2021-06-30 22:44:03 +0200

    save directus settings to localstorage

--- pages/api/config/settings.js
+++ pages/api/config/settings.js
@@ -13,19 +13,40 @@ const colors = {
 
 export default async function handler(req, res) {
   const nearestHumanColor = require("nearest-human-color").from(colors);
+  const settings = localstorage.getItem("settings");
+  let responseReturned = false;
+
+  if (settings) {
+    responseReturned = true;
+    const storedSettings = JSON.parse(settings);
+    const themeColor = nearestHumanColor(storedSettings.project_color).name;
+
+    res.status(200).json({
+      name: storedSettings.project_name || "Project",
+      logo: storedSettings.project_logo ? `${process.env.API_URL}/assets/${storedSettings.project_logo}` : `https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg`,
+      color: storedSettings.project_color,
+      themeColor,
+      customCSS: storedSettings.custom_css,
+      url: storedSettings.project_url
+    });
+  }
 
   directus.settings.readMany()
     .then(({ data }) => {
-      const themeColor = nearestHumanColor(data.project_color).name;
+      localstorage.setItem("settings", JSON.stringify(data));
 
-      res.status(200).json({
-        name: data.project_name || "Project",
-        logo: data.project_logo ? `${process.env.API_URL}/assets/${data.project_logo}` : `https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg`,
-        color: data.project_color,
-        themeColor,
-        customCSS: data.custom_css,
-        url: data.project_url
-      });
+      if (!responseReturned) {
+        const themeColor = nearestHumanColor(data.project_color).name;
+
+        res.status(200).json({
+          name: data.project_name || "Project",
+          logo: data.project_logo ? `${process.env.API_URL}/assets/${data.project_logo}` : `https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg`,
+          color: data.project_color,
+          themeColor,
+          customCSS: data.custom_css,
+          url: data.project_url
+        });
+      }
     })
     .catch(res.status(404).json);
 }
