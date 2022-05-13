b3c45b43d343bc1ca987db326f10136d31a315e7
Calderis <r.wetteren@gmail.com>
2022-05-13 10:33:55 +0200

    Fix: User fetch syntax

--- pages/api/users/search.js
+++ pages/api/users/search.js
@@ -1,7 +1,7 @@
 import { directus } from "/utils/directus";
 
 export default async function handler(req, res) {
-  directus.users.readMany(req.query)
+  directus.users.readByQuery(req.query)
     .then(data => res.status(200).json(data))
     .catch(res.status(401).json);
 }
